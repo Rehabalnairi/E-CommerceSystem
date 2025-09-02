042a4e55b1143634970c5c54379184fdf9d5e680
Thuraiya <3eya151@gmail.com>
2025-09-02 15:44:03 +0400

    create SupplierService an ISupplierService

---
+++ Services/ISupplierService.cs
@@ -0,0 +1,14 @@
+﻿using E_CommerceSystem.Models;
+
+namespace E_CommerceSystem.Services
+{
+    public interface ISupplierService
+    {
+        void AddSupplier(Supplier supplier);
+        void DeleteSupplier(int supplierId);
+        IEnumerable<Supplier> GetAllSuppliers(int pageNumber, int pageSize, string? name = null);
+        Supplier GetSupplierById(int supplierId);
+        Supplier GetSupplierByName(string supplierName);
+        void UpdateSupplier(Supplier supplier);
+    }
+}
