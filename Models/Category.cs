88538aa0f1137e8cc0cd013fc6cca32b4f258466
Rehabalnairi <133367687+Rehabalnairi@users.noreply.github.com>
2025-09-02 13:49:51 +0400

    create models Category

---
+++ Models/Category.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace E_CommerceSystem.Models
+{
+    public class Category
+    {
+        [Key]
+        public int CategoryId { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        // Relationship: One Category → Many Products
+        [JsonIgnore]
+        public virtual ICollection<Product> Products { get; set; }
+    }
+}
