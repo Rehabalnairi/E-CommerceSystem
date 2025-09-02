c65b8a3e6fa01f5069c890e902ea11ba547b97c5
Rehabalnairi <133367687+Rehabalnairi@users.noreply.github.com>
2025-09-02 14:45:21 +0400

    create DTO

---
+++ Models/CategoryCreateDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace E_CommerceSystem.Models
+{
+    public class CategoryCreateDTO
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ Models/CategoryDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace E_CommerceSystem.Models
+{
+    public class CategoryDTO
+    {
+        public int CategoryId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
