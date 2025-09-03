c688895ddcea5928a65a733e0a9684a21d288d8f
Thuraiya <3eya151@gmail.com>
2025-09-03 09:42:24 +0400

    Implement Order Summary Service New service that gathers data from Orders, Products, and Users

--- Mapping/AutoMapperProfile.cs
+++ Mapping/AutoMapperProfile.cs
@@ -18,6 +18,16 @@ public class AutoMapperProfile : Profile
         CreateMap<Supplier, SupplierDTO>().ReverseMap();
         CreateMap<SupplierDTO, Supplier>();
 
+        CreateMap<Order, OrderSummaryDTO>()
+        .ForMember(dest => dest.OID, opt => opt.MapFrom(src => src.OID))
+        .ForMember(dest => dest.UName, opt => opt.MapFrom(src => src.user.UName))
+        .ForMember(dest => dest.Items, opt => opt.MapFrom(src => src.OrderProducts));
+
+        CreateMap<OrderProducts, OrderItemDTO>()
+            .ForMember(dest => dest.ProductId, opt => opt.MapFrom(src => src.PID))
+            .ForMember(dest => dest.ProductName, opt => opt.MapFrom(src => src.product.ProductName))
+            .ForMember(dest => dest.Quantity, opt => opt.MapFrom(src => src.Quantity));
 
     }
 }
+
